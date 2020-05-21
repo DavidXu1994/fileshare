66ed2f88b8048c60b27d07c256a400bc6f353f01
david_xu <1148122993@qq.com>
2020-05-22 03:10:23 +0800

    表单提交

--- src/main/java/com/bysj/fileshare/controller/DocumentInfoController.java
+++ src/main/java/com/bysj/fileshare/controller/DocumentInfoController.java
@@ -5,6 +5,9 @@ import com.bysj.fileshare.service.DocumentInfoService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,11 +37,17 @@ public class DocumentInfoController {
      *
      * @return
      */
-    @RequestMapping("/file/list/query")
-    public ModelAndView queryFileList() {
+    @PostMapping("/file/list/query")
+    public ModelAndView queryFileList(Model model) {
+        model.addAttribute("DocumentInfoVo", new DocumentInfoVo());
         List<DocumentInfoVo> ls=  documentInfoService.queryFileList();
         ModelAndView mav = new ModelAndView("demo");
         mav.addObject("list", ls);
         return mav;
+    }
+
+    @GetMapping("/index/query")
+    public ModelAndView index() {
+        return new ModelAndView("index");
     }
 }
